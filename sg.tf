1568b3fe9a0d9ff0d0262bc98eae8dcb0df814ba
DevenRaut <91963968+DevenRaut@users.noreply.github.com>
2024-08-11 12:11:32 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -42,6 +42,14 @@ ingress {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
